31bfb3bb93fd30f056fa6c2f27466e4e8c77f323
Anton <140256738+NeoSkyline@users.noreply.github.com>
2024-10-20 21:45:35 +0300

    Update 2.8.java

--- 2.8.java
+++ 2.8.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
     public class Main {
         public static void main(String[] args) {
-            eightTask();j
+            eightTask();
         }
         public static void eightTask() {
             Scanner scanner = new Scanner(System.in);
